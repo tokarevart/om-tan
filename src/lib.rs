010249211207471bdab51b47da3d0fb8575e0507
Tokarev Artyom <tokarev28.art@gmail.com>
2020-08-28 00:53:26 +0500

    upd

--- src/lib.rs
+++ src/lib.rs
@@ -25,3 +25,29 @@ pub fn search(range: Range<f64>, eps: f64, f: impl Fn(f64) -> f64, der: impl Fn(
 
     x
 }
+
+pub fn search_with_n(range: Range<f64>, n: usize, f: impl Fn(f64) -> f64, der: impl Fn(f64) -> f64) -> f64 {
+    assert!(n > 0);
+
+    let mut a = range.start;
+    let mut b = range.end;
+    let mut dera = der(a);
+    let mut derb = der(b);
+    let mut x = (f(a) - f(b) + b * derb - a * dera) / (derb - dera);
+    let mut derx = der(x);
+
+    for _ in 0..n {
+        if derx < 0.0 {
+            a = x;
+            dera = derx;
+        } else {
+            b = x;
+            derb = derx;
+        }
+
+        x = (f(a) - f(b) + b * derb - a * dera) / (derb - dera);
+        derx = der(x);
+    }
+
+    x
+}
